6ee2a3b00340ebf3bba9fc098f9f3f74db815832
MikaelShaikh6 <mikael2007s@gmail.com>
2026-09-07 23:18:27 -0400

    Add task CRUD API

--- server/src/routes/tasks.ts
+++ server/src/routes/tasks.ts
@@ -34,7 +34,54 @@ router.post("/", async (req, res) => {
     res.status(201).json(result.rows[0]);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: String(error) });
+    res.status(500).json({ error: "Failed to add task" });
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { completed } = req.body;
+
+    const result = await pool.query(
+      `UPDATE tasks
+       SET completed = $1
+       WHERE id = $2
+       RETURNING *`,
+      [completed, id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to update task" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+        
+    const result = await pool.query(
+      `DELETE FROM tasks
+      WHERE id = $1
+      RETURNING *`,
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Task not found"});
+    }
+
+    res.json(result.rows[0]);
+  }
+  catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to delete task" })
   }
 });
 
